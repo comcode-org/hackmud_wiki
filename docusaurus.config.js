b5706fe01f0bff1b0e212ef1f38005b227fb3597
Daniel Swann <swann.dp@gmail.com>
2023-09-29 01:12:03 -0400

    Initial Docusaurus skeleton (#2)
    * Basic package setup
    
    * Install `prettier` with default options
    
    * Basic Docusaurus config
    
    * Configure prettier with Docusaurus and run it
    
    * Add White Rabbit font
    
    * Add links to config object definitions
    
    * Small changes to homepage to show nested ToC
    
    * Typechecking support
    
    * Prevent GH pages from discarding _-prefixed files
    
    * Remove command to avoid manual deploys to GH Pages
    
    * Revert "Add White Rabbit font"
    
    This reverts commit a6c03f21b17f770b1ff7cedac4c03f3041f51c2d.
    
    * Lock in typescript version
    
    * Requested changes to docusaurus.config.js
    
    Remove TODO, remove block comments (except for JSDoc types), address ambiguous comments
    
    * Dependency reorganization.
    
    Make any packages required for prod / dev tooling to run correctly an explicit dependency. This left only "*prettier*" as the last public hoist, which is already part of the default value in pnpm.
    
    * Increase node req to >=20.x

---
+++ docusaurus.config.js
@@ -0,0 +1,85 @@
+// @ts-check
+
+const GITHUB_ORG = "comcode-org";
+const GITHUB_PROJECT = "hackmud_wiki";
+
+// See: https://docusaurus.io/docs/api/docusaurus-config
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  // Metadata
+  title: "hackmud Wiki",
+  tagline: "Knowledge repository for the video game hackmud",
+  //favicon: 'img/favicon.ico',
+
+  // Deployment details
+  url: `https://${GITHUB_ORG}.github.io`,
+  baseUrl: `/${GITHUB_PROJECT}/`,
+
+  // GitHub Pages config for CLI deployment
+  organizationName: GITHUB_ORG,
+  projectName: GITHUB_PROJECT,
+
+  // Internationalization options, useful to set even if you only support one
+  // language because it generates the `lang` attribute on the `html` tag
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  // Installed plugins
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      // See: https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-docs#configuration
+      {
+        // Path of the docs plugin relative to the root. Since it's the only
+        // plugin we're using, put it at the top
+        routeBasePath: "/",
+        // Base url for "Edit This Page" button on content pages
+        editUrl: `https://github.com/${GITHUB_ORG}/${GITHUB_PROJECT}/edit/main`,
+        // Path to custom sidebar definition(s)
+        //sidebarPath: require.resolve('./sidebar.js'),
+        // Plugins for remark, at the Markdown AST level
+        remarkPlugins: [],
+        // Plugins for rehype, at the HTML AST level
+        rehypePlugins: [],
+      },
+    ],
+  ],
+
+  // Installed themes
+  themes: [
+    [
+      "@docusaurus/theme-classic",
+      // See: https://docusaurus.io/docs/api/themes/@docusaurus/theme-classic#configuration
+      {
+        customCss: require.resolve("./src/css/custom.css"),
+      },
+    ],
+  ],
+
+  // Global theme config
+  // See: https://docusaurus.io/docs/api/themes/configuration
+  /** @type {import('@docusaurus/theme-common').UserThemeConfig} */
+  themeConfig: {
+    // Supported and default color modes
+    colorMode: {
+      defaultMode: "dark",
+    },
+    // Theme configuration specific to the docs plugin
+    docs: {
+      sidebar: {
+        autoCollapseCategories: true,
+      },
+    },
+    navbar: {
+      title: "hackmud Wiki",
+    },
+    footer: {
+      copyright: `Copyright © ${new Date().getFullYear()} ComCODE`,
+      links: [],
+    },
+  },
+};
+
+module.exports = config;
