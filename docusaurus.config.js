d0e605abaff9fc3cdd84e99ecf98001fd89b7f87
Sean Mann <sean@drizzlybear.com>
2023-10-02 09:39:43 -0700

    lintfix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,9 +77,9 @@ const config = {
     },
     footer: {
       logo: {
-        alt: 'License: CC BY-NC-SA 4.0',
-        src: 'https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg',
-        href: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+        alt: "License: CC BY-NC-SA 4.0",
+        src: "https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg",
+        href: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
       },
       copyright: `Licensed for creative and educational use by ComCODE`,
     },
