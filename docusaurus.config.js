b590da91aa0b0760bb493eb4b4405df105c1fa0e
seanmakesgames <sean@comcode.org>
2023-10-03 03:16:35 +0000

    weird. autoformat was not working

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,8 @@ const config = {
       title: "hackmud Wiki",
     },
     footer: {
-      copyright: 'Licensed by ComCODE with <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a> for creative and educational use.',
+      copyright:
+        'Licensed by ComCODE with <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a> for creative and educational use.',
     },
   },
 };
