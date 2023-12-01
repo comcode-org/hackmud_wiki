56754a02a264911786fd8397d7e8ab1cdaa6666c
tukib <ward.leikis@gmail.com>
2023-12-02 02:21:01 +1300

    created plugin to wrap plugin-content-docs

---
+++ src/plugins/pluginContentDocsWrapper.ts
@@ -0,0 +1,96 @@
+import pluginContentDocs, {
+  LoadedContent,
+  LoadedVersion,
+  PluginOptions,
+} from "@docusaurus/plugin-content-docs";
+import { LoadContext, Plugin } from "@docusaurus/types";
+import remarkWikiLink from "remark-wiki-link";
+
+function getCurrentVersion(content: LoadedContent): LoadedVersion {
+  return content.loadedVersions.find(
+    (version) => version.versionName === "current",
+  );
+}
+
+function getConfiguredWikiLinkPlugin(
+  content: LoadedContent,
+): [Function, object] {
+  const docs = getCurrentVersion(content).docs;
+  return [
+    remarkWikiLink,
+    {
+      // class attached to all generated links
+      wikiLinkClassName: "wikilink",
+
+      // class attached to resolved links not found in permalinks
+      newClassName: "wikilink-new",
+
+      // list of permalinks that exist
+      permalinks: docs.map((doc) => doc.permalink),
+
+      // href path that the plugin generates based on the permalink
+      hrefTemplate: function (permalink: string): string {
+        return permalink;
+      },
+
+      /**
+       * 1. A wikilink can contain either a page name or slug.
+       *    - e.g. `[[Name]]` `[[slug/path]]` `[[Alias:...]]`
+       *
+       * 2. The resolver will return a permalink for a page where the link name
+       *    is the title or the link slug is a suffix* of the permalink.
+       *    - *bar/baz matches foo/bar/baz but not foobar/baz
+       *
+       * 3. The resolver will return a permalink for a page at the highest
+       *    directory level possible.
+       *    - e.g. bar/baz matches /foo/bar/baz but prefers /bar/baz if present
+       *
+       * 4. The resolver will return the slug instead if no matching docs exist.
+       */
+      pageResolver: function (pageName: string): string[] {
+        const name = pageName.toLowerCase();
+        const slugReference = name[0] === "/" ? name : "/" + name;
+
+        const relevantDocs = docs.filter(
+          (doc) =>
+            doc.title.toLowerCase() === name ||
+            doc.permalink.endsWith(slugReference),
+        );
+
+        if (relevantDocs.length <= 0) {
+          return [slugReference];
+        }
+
+        const topLevelRelevantDoc = relevantDocs.reduce((docA, docB) => {
+          const depthA = docA.permalink.split("/").length;
+          const depthB = docB.permalink.split("/").length;
+          return depthA <= depthB ? docA : docB;
+        });
+
+        return [topLevelRelevantDoc.permalink];
+      },
+    },
+  ];
+}
+
+/**
+ * Returns a copy of the `@docusaurus/plugin-content-docs` plugin.
+ *
+ * The `contentLoaded` method is overridden to inject the `remark-wiki-link`
+ * plugin and a configuration for it.
+ */
+export default async function pluginContentDocsWrapper(
+  context: LoadContext,
+  options: PluginOptions,
+): Promise<Plugin<LoadedContent>> {
+  const originalPlugin = await pluginContentDocs(context, options);
+  return {
+    ...originalPlugin,
+    contentLoaded: function (args) {
+      options.remarkPlugins.push(getConfiguredWikiLinkPlugin(args.content));
+      return originalPlugin.contentLoaded(args);
+    },
+  };
+}
+
+export { validateOptions } from "@docusaurus/plugin-content-docs/lib/options.js";
