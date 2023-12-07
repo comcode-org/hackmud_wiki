60c36b049438f93ada492619b1efe2dea9034195
seanmakesgames <sean@comcode.org>
2023-12-07 00:38:20 +0000

    remove unintentional add of links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,7 +115,6 @@ const config = {
     footer: {
       copyright:
         'Licensed by ComCODE with <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a> for creative and educational use.',
-      links: [],
     },
   },
 
