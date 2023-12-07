ba8f6d21dfce3d27b5a4595da48ef2b8302124b3
seanmakesgames <sean@comcode.org>
2023-12-06 16:36:15 -0800

    revert footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,7 +113,8 @@ const config = {
       ],
     },
     footer: {
-      copyright: `Copyright © ${new Date().getFullYear()} ComCODE`,
+      copyright:
+        'Licensed by ComCODE with <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a> for creative and educational use.',
       links: [],
     },
   },
