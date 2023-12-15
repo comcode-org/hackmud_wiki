421665aaa890b31ad393d392ccd57a086afa068f
Bryn Ward-Leikis <ward.leikis@gmail.com>
2023-12-16 12:47:35 +1300

    refine jest configuration (#72)
    * add mjs to jest and stricter testMatch globs
    
    * switch jest config from CJS to ESM
    
    * test coverage report
    
    * Revert out of scope changes

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/**/*.{[jt]s?(x),mjs}",
+    "**/*(*.)+(spec|test).{[jt]s?(x),mjs}",
+  ],
 };
