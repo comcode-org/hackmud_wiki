ed5b94a179808d6de9bb8f5bb5411a9b0b667d72
kbinreallife <141305609+kbinreallife@users.noreply.github.com>
2024-02-12 15:15:14 -0800

    Create clamp.js

---
+++ docs/scripting/scripts.lib/clamp.js
@@ -0,0 +1,29 @@
+---
+title: scripts.lib().clamp()
+---
+
+### Description
+
+## Syntax
+
+
+
+### Parameters
+
+A list of parameters and their descriptions if applicable
+
+### Return
+
+What the function or script returns
+
+#### CLI
+
+return here
+
+#### Script
+
+return here
+
+## Example
+
+An example use of the function, in code, or script, in client and code
