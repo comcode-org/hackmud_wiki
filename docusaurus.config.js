aec3d8f2c17b13131d46325be002380350e6353f
seanmakesgames <sean@comcode.org>
2024-03-26 23:23:05 +0000

    my fav. icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   // Metadata
   title: "hackmud Wiki",
   tagline: "Knowledge repository for the video game hackmud",
-  favicon: 'assets/favicon.ico',
+  favicon: "favicon.ico",
 
   // Deployment details
   url: `https://wiki.hackmud.com`,
