c036b30b707d2f8c62e798f58304ea7b8bac7edd
dayofni <98456413+dayofni@users.noreply.github.com>
2024-03-28 16:07:48 +1030

    added hackmud color codes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -139,6 +139,10 @@ const config = {
               type: "docSidebar",
               sidebarId: "databaseSidebar",
             },
+            {
+              type: "docSidebar",
+              sidebarID: "miscSidebar"
+            }
           ],
         },
         {
