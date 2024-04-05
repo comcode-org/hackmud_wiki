dfbe38fe3604b647a5faa245e132e8362f06eb32
Gergő Móricz <mo.geryy@gmail.com>
2024-04-05 19:56:40 +0200

    feat(guides): Add Chat API docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -153,6 +153,10 @@ const config = {
               type: "docSidebar",
               sidebarId: "newPlayersSidebar",
             },
+            {
+              type: "docSidebar",
+              sidebarId: "advancedSidebar",
+            }
           ],
         },
       ],
