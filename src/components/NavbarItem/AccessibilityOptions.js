e8050aa7f6930e814fb99c2664aba637e516bd25
matr1x-hackmud <matr1x.hackmud@gmail.com>
2025-05-27 14:09:23 +0100

    Lint

--- src/components/NavbarItem/AccessibilityOptions.js
+++ src/components/NavbarItem/AccessibilityOptions.js
@@ -2,21 +2,37 @@ import React from "react";
 
 export default function AccessibilityOptions() {
   function handleClick() {
-    let currentA11yOption = window.localStorage.getItem('accessibilityMode');
+    let currentA11yOption = window.localStorage.getItem("accessibilityMode");
 
-    window.localStorage.setItem('accessibilityMode', (currentA11yOption === "true" ? "false" : "true") );
-    currentA11yOption = (currentA11yOption === "true" ? "false" : "true");
+    window.localStorage.setItem(
+      "accessibilityMode",
+      currentA11yOption === "true" ? "false" : "true",
+    );
+    currentA11yOption = currentA11yOption === "true" ? "false" : "true";
 
     document.documentElement.setAttribute("data-accessible", currentA11yOption);
-    for(let elem of document.getElementsByClassName('main-wrapper')) elem.setAttribute("data-accessible", currentA11yOption);
+    for (let elem of document.getElementsByClassName("main-wrapper"))
+      elem.setAttribute("data-accessible", currentA11yOption);
   }
 
   function a11yOnLoad() {
-    let currentA11yOption = window.localStorage.getItem('accessibilityMode');
+    let currentA11yOption = window.localStorage.getItem("accessibilityMode");
 
-    document.documentElement.setAttribute("data-accessible", (currentA11yOption || "false"));
-    for(let elem of document.getElementsByClassName('main-wrapper')) elem.setAttribute("data-accessible", currentA11yOption);
+    document.documentElement.setAttribute(
+      "data-accessible",
+      currentA11yOption || "false",
+    );
+    for (let elem of document.getElementsByClassName("main-wrapper"))
+      elem.setAttribute("data-accessible", currentA11yOption);
   }
 
-  return <img src class="navbar-a11y" aria-label="Enable or disable accessible reading mode" onError={a11yOnLoad} onClick={handleClick}></img>;
+  return (
+    <img
+      src
+      class="navbar-a11y"
+      aria-label="Enable or disable accessible reading mode"
+      onError={a11yOnLoad}
+      onClick={handleClick}
+    ></img>
+  );
 }
