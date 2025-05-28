5fec4d7a428cb1eb955d29a5c8ed239f458d587c
matr1x-hackmud <matr1x.hackmud@gmail.com>
2025-05-28 14:26:28 +0100

    Remake light-mode to work with the new accessibility toggle

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
     // Supported and default color modes
     colorMode: {
       defaultMode: "dark",
-      disableSwitch: true,
+      disableSwitch: false,
       respectPrefersColorScheme: false,
     },
     // Theme configuration specific to the docs plugin
